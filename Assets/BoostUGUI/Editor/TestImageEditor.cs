921f988fefa9166f30d3d1527dbfc1ea745b519d
Leezard <lfj200411@163.com>
2017-12-28 20:43:18 +0800

    ...

---
+++ Assets/BoostUGUI/Editor/TestImageEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+
+namespace BoostUGUI.Editor {
+    [CustomEditor(typeof(TestImage))]
+    public class TestImageEditor : UnityEditor.Editor {
+        private SerializedProperty _spriteAtlas;
+        private SerializedProperty _spriteName;
+
+        private void OnEnable() {
+            _spriteAtlas = serializedObject.FindProperty("SpriteAtlas");
+            _spriteName = serializedObject.FindProperty("SpriteName");
+        }
+
+        public override void OnInspectorGUI() {
+            EditorGUILayout.PropertyField(_spriteAtlas);
+            EditorGUILayout.PropertyField(_spriteName);
+        }
+    }
+}
---
+++ Assets/BoostUGUI/TestImage.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine.UI;
+
+namespace BoostUGUI {
+    public class TestImage : Image {
+        public SpriteAtlas SpriteAtlas;
+        public string SpriteName;
+
+        protected override void Awake() {
+            sprite = SpriteAtlas.GetSprite(SpriteName);
+        }
+    }
+}
