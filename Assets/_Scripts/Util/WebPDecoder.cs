0e1de3a3e3e2659452860ec6aab92f2f523b2aec
Leezard <lfj200411@163.com>
2018-01-01 21:30:28 +0800

    update dll

--- Assets/_Scripts/Util/WebPDecoder.cs
+++ Assets/_Scripts/Util/WebPDecoder.cs
@@ -16,18 +16,40 @@ namespace _Scripts.Util {
 
         //WEBP_EXTERN(void) WebPFree(void* ptr);
         [DllImport("webpdecoder")]
-        private static extern void WebPSafeFree(IntPtr intPtr);
+        private static extern void WebPFree(IntPtr intPtr);
+
+//        public static Texture2D DecodeFromBytes(byte[] bytes) {
+//            IntPtr webpData = Marshal.AllocHGlobal(bytes.Length);
+//            Marshal.Copy(bytes, 0, webpData, bytes.Length);
+//            int width, height;
+//            IntPtr rgbaData = WebPDecodeRGBA(webpData, bytes.Length, out width, out height);
+//            Marshal.FreeHGlobal(webpData);
+//            Texture2D texture2D = new Texture2D(width, height, TextureFormat.RGBA32, false);
+//            byte[] textureBytes = new byte[width * height * 4];
+//            Marshal.Copy(rgbaData, textureBytes, 0, textureBytes.Length);
+//            WebPFree(rgbaData);
+//            texture2D.LoadRawTextureData(textureBytes);
+//            texture2D.Apply();
+//            return texture2D;
+//        }
+
+        //WEBP_EXTERN uint8_t* WebPCustomDecode(const uint8_t* data, size_t data_size, int* width, int* height, int down_scaling, int mode);
+        [DllImport("webpdecoder")]
+        private static extern IntPtr WebPCustomDecode(IntPtr data, int dataLength, out int width, out int height,
+            int downScaling, int mode);
 
         public static Texture2D DecodeFromBytes(byte[] bytes) {
             IntPtr webpData = Marshal.AllocHGlobal(bytes.Length);
             Marshal.Copy(bytes, 0, webpData, bytes.Length);
             int width, height;
-            IntPtr rgbaData = WebPDecodeRGBA(webpData, bytes.Length, out width, out height);
+            IntPtr rgbaData = SystemInfo.maxTextureSize < 2048
+                ? WebPCustomDecode(webpData, bytes.Length, out width, out height, 1, 1)
+                : WebPCustomDecode(webpData, bytes.Length, out width, out height, 0, 1);
             Marshal.FreeHGlobal(webpData);
             Texture2D texture2D = new Texture2D(width, height, TextureFormat.RGBA32, false);
             byte[] textureBytes = new byte[width * height * 4];
             Marshal.Copy(rgbaData, textureBytes, 0, textureBytes.Length);
-            WebPSafeFree(rgbaData);
+            WebPFree(rgbaData);
             texture2D.LoadRawTextureData(textureBytes);
             texture2D.Apply();
             return texture2D;
